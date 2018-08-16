8b1a290deca434b322387e4037c4277b5653cd72
alemangka <01080039210@naver.com>
2018-08-16 13:52:28 +0900

    first

---
+++ config/sample_seoul_key.js
@@ -0,0 +1,8 @@
+var seoul_api = {
+	api_key : '6d517a6363616c6537324274677975',
+	api_url : 'http://openAPI.seoul.go.kr:8088/',
+	api_name :'SearchPerformanceBySubjectService'
+		
+}
+
+exports.seoul = seoul_api
